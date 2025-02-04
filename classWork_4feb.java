05ba67e5484b6e489c14e6c467f9586a58f1017c
irshadwnl <irshadwnl07@gmail.com>
2025-02-04 11:44:37 +0530

    Added Straight Line comparison  coding question

--- classWork_4feb.java
+++ classWork_4feb.java
@@ -24,20 +24,17 @@ import java.util.*;
 public class classWork_4feb {
 
   public static void main(String[] args) {
-    String str = "this is a word and i m a legend";
-    String s = "this is my second sgtring";
-    int len1 = 0;
-    int len2 = 0;
-    for (char ch : str.toCharArray()) {
-      len1++;
-    }
-    for (char ch : s.toCharArray()) {
-      len2++;
-    }
-    if (len1 > len2) {
-      System.out.print(str);
-    } else {
-      System.out.print(str);
-    }
+    Scanner sc=new Scanner(System.in);
+        int x1=sc.nextInt();
+        int y1=sc.nextInt();
+        int x2=sc.nextInt();
+        int y2=sc.nextInt();
+         double d1 = Math.sqrt(x1*x1 + y1*y1);
+        double d2 = Math.sqrt(x2*x2 + y2*y2);
+        if (d1 > d2) {
+            System.out.println("(" + x1 + ", " + y1 + ")");
+        } else {
+            System.out.println("(" + x2 + ", " + y2 + ")");
+        }
   }
 }
